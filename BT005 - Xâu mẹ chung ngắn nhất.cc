58585ee72b17778243657a8a3e3fe150e6ffaeb7
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2025-03-09 01:25:13 +0700

    Xâu mẹ chung ngắn nhất

---
+++ BT005 - Xâu mẹ chung ngắn nhất.cc
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    int t; cin >> t;
+    while(t--) {
+        string s1,s2;
+        cin >> s1 >> s2;
+        int n1 = s1.size(), n2 = s2.size();
+        int dp[n1+1][n2+1];
+        for(int i = 0; i<=n1; i++){
+            dp[i][0] = 0;
+        }
+        for(int j = 0; j<=n2; j++){
+            dp[0][j] = 0;
+        }
+        for(int i = 1; i<=n1;i++){
+            for(int j = 1; j<=n2; j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        cout << n1+n2 - dp[n1][n2] << endl;
+    }
+    return 0;
+}
