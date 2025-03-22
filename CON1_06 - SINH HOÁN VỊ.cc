7df49539e9059cc41d7d5440931ae5b8fef89fb7
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2025-03-22 20:20:55 +0700

    SINH HOAN VI

--- CON1_06 - SINH HOÁN VỊ.cc
+++ CON1_06 - SINH HOÁN VỊ.cc
@@ -4,7 +4,8 @@ int main(){
     int t; cin >> t;
     while(t--){
         int n; cin >> n;
-        for(int i = 0; i< n; i++) a[i] = i++;
+        int a[n];
+        for(int i = 0; i< n; i++) a[i] = i+1;
         do{
             for(int i = 0; i<n; i++){
                 cout << a[i];
