c9a7db4f9b3bb658734f64cba521da4f81468db9
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2025-03-22 17:24:27 +0700

    Hoan Vi Ke Tiep

---
+++ CON1_03 - HOÁN VỊ KẾ TIẾP.cc
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, a[1005];
+void next(){
+    int i = n - 1;
+    while(i > 0 && a[i] > a[i+1]){
+        i--;
+    }
+    if(i == 0){
+        for(int j = 1; j<=n; j++){
+            cout << j << " ";
+        }
+    }
+    else{
+        int j = n;
+        while(a[i] > a[j]) j--;
+        swap(a[i], a[j]);
+        reverse(a + i + 1, a + n + 1);
+        for(int i = 1; i<= n; i++) {
+            cout << a[i] << " ";
+        }
+    }
+    cout<<endl;
+}
+int main(){
+    int t; cin >> t;
+    while (t--)
+    {
+        /* code */
+        cin >> n;
+        for(int i = 1; i<= n; i++) cin >> a[i];
+        next();
+    }
+    return 0;
+}
