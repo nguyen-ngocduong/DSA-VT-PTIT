1052f6c43812e02a00c50de562cf688e24ad6538
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2025-03-26 13:57:55 +0700

    Fix Quick Sort

--- quick_sort.cc
+++ quick_sort.cc
@@ -1,35 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-int partition(int left, int right, int a[]){
-    int pointer = left;
+int patition(int a[], int left, int right){
     int pivot = a[right];
+    int pointer = left;
     for(int i = left; i<= right; i++){
-        if(a[i] <= pivot){
+        if(a[i] < pivot) {
             swap(a[i], a[pointer]);
             pointer += 1;
         }
     }
-    swap(a[pointer], a[right]);
+    swap(a[right], a[pointer]);
     return pointer;
 }
-void quick_sort(int left, int right, int a[]){
+void quick_sort(int a[], int left, int right){
     if(left < right){
-        int p = partition(left, right, a);
-        quick_sort(left, p-1,a);
-        quick_sort(p+1, right,a);
+        int tmp = patition(a,left,right);
+        quick_sort(a,left, tmp - 1);
+        quick_sort(a,tmp+1, right);
     }
 }
 int main(){
-    int tc; cin>>tc;
-    while(tc--) {
-        int n; cin>>n;
+    int t; cin >>t;
+    while(t--){
+        int n; cin >> n;
         int a[n];
-        for(int i = 0; i<n;i++){
-            cin >> a[i];
+        for(int i = 0; i<n; i++){
+            cin>>a[i];
         }
-        quick_sort(0,n-1,a);
-        for(int i=0;i<n-1;i++){
-            cout << a[i]<< " ";
+        quick_sort(a,0,n-1);
+        for(int i = 0; i<n; i++){
+            cout << a[i] << ' ';
         }
         cout << endl;
     }
