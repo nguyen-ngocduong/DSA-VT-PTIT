5650c4e35073306a2db7ccd8a340071ba63aba3b
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2025-04-27 19:45:48 +0700

    BFS TRÊN ĐỒ THỊ CÓ HƯỚNG

---
+++ CON9_09 - BFS trên đồ thị có hướng.cc
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int v,e,u,visited[1001];
+vector<int> ke[1001];
+void init(){
+    cin >> v >> e >> u;
+    memset(visited,0,sizeof(visited));
+    for(int i = 0; i<e; i++){
+        int x,y;
+        cin >> x >> y;
+        ke[x].push_back(y);
+    }
+}
+void BFS(int u){
+    queue<int> q;
+    q.push(u);
+    visited[u] = 1;
+    while(!q.empty()){
+        int s = q.front();
+        q.pop();
+        cout << s << " ";
+        for(int x : ke[s]){
+            if(!visited[x]){
+                visited[x] = 1;
+                q.push(x);
+            }
+        }
+    }
+}
+int main(){
+    int t; cin >> t;
+    while(t--){
+        init();
+        BFS(u);
+        cout << endl;
+    }
+    return 0;
+}
