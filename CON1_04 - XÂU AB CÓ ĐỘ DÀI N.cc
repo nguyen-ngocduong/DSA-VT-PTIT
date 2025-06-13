24ad82969eaf1f1213d3beac0127eee9afd112eb
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2025-06-14 01:29:36 +0700

    Xau AB co do dai n

--- CON1_04 - XÂU AB CÓ ĐỘ DÀI N.cc
+++ CON1_04 - XÂU AB CÓ ĐỘ DÀI N.cc
@@ -32,3 +32,34 @@ int main(){
     }
     return 0;
 }
+/* c2
+#include<bits/stdc++.h>
+using namespace std;
+char a[1001];
+int ok;
+void next(int n){
+    int i = n;
+    while(i > 0 && a[i] == 'B'){
+        a[i] = 'A';
+        i--;
+    }
+    if(i == 0) ok = 0;
+    else a[i] = 'B';
+}
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        memset(a,'A', sizeof(a));
+        ok = 1;
+        while(ok){
+            for(int i = 1; i<= n; i++){
+                cout << a[i];
+            }
+            cout << ' ';
+            next(n);
+        }
+        cout << endl;
+    }
+    return 0;
+}*/
