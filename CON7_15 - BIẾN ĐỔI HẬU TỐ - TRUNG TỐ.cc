20a49f81f5e3c6d2a0666cbd6ab25ee6b7492e89
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2025-06-14 01:28:44 +0700

    Bien Doi Hau To - Trung To

--- CON7_15 - BIẾN ĐỔI HẬU TỐ - TRUNG TỐ.cc
+++ CON7_15 - BIẾN ĐỔI HẬU TỐ - TRUNG TỐ.cc
@@ -1,19 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main (){
+int main(){
     int t; cin >> t;
     while(t--){
         string s; cin >> s;
         stack<string> st;
-        for(int i = 0; i< s.size(); i++){
-            string x = string(1, s[i]);
-            if (x == "+" || x == "-" || x == "*" || x == "/") {
-                string b = st.top(); st.pop();
-                string a = st.top(); st.pop();
-                string c = "(" + a + x + b + ")";
-                st.push(c);
+        for(int i = 0; i<s.size(); i++){
+            if(s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/'){
+                string tmp1 = st.top();
+                st.pop();
+                string tmp2 = st.top();
+                st.pop();
+                string res = "(" + tmp2 + s[i] + tmp1 + ")";
+                st.push(res);
             }
-            else st.push(x);
+            else st.push(string(1,s[i]));
         }
         cout << st.top() << endl;
     }
