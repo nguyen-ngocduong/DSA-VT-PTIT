5d9a74281b1c0f513cf384d27a4a8f440f588397
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2025-06-15 16:44:50 +0700

    Kiem Tra Chu Trinh Su Dung DIJSON SET

--- CON9_26 - KIỂM TRA CHU TRÌNH SỬ DỤNG DISJOIN SET.cc
+++ CON9_26 - KIỂM TRA CHU TRÌNH SỬ DỤNG DISJOIN SET.cc
@@ -1,45 +1,35 @@
 #include<bits/stdc++.h>
-#define maxn 100
 using namespace std;
-int n, check[maxn], cycle;
-int a[maxn][maxn];
-
-void dfs(int u, int p)
-{
-	check[u]=1;
-	for(int v=1;v<=n;v++)
-	{
-		if(a[u][v])
-		{
-			if(!check[v]) dfs(v,u);
-			else if(v!=p) cycle=1;
-		}
-	}
+int v,e,visited[1001], cycle;
+int a[1001][1001];
+void dfs(int u, int p){//u la dinh hien tai, p la dinh cha cua u
+    visited[u] = 1;
+    for(int i = 1; i<=v; i++){
+        if(a[u][i]){// neu co canh noi giua dinh u va i
+            if(!visited[i]){
+                dfs(i, u);
+            }
+            else if(i != p){ // neu i duoc tham => ktra xem i co khac p 0
+                cycle = 1;
+            }
+        }
+    }
 }
-void solution()
-{
-	cin >> n;
-	memset(a,0,sizeof(a));
-	memset(check,0,sizeof(check));
-	cycle=0;
-	for(int i=1;i<n;i++)
-	{
-		int x, y;
-		cin >> x >> y;
-		a[x][y]=1;
-		a[y][x]=1;
-	}
-	dfs(1,0);
-	if(cycle) cout << "NO";
-	else cout << "YES";
-}
-main()
-{
-	int t;
-	cin >> t;
-	while(t--)
-	{
-		solution();
-		cout << endl;
-	}
+int main(){
+    int t; cin >> t;
+    while(t--){
+        cin >> v >> e;
+        memset(visited,0,sizeof(visited));
+        memset(a,0,sizeof(a));
+        for(int i = 1; i<= e; i++){
+            int x,y; cin >> x >> y;
+            a[x][y] = 1;
+            a[y][x] = 1;
+        }
+        cycle = 0;
+        dfs(1,0);
+        if(cycle == 0) cout << "NO" << endl;
+        else cout << "YES" << endl;
+    }
+    return 0;
 }
